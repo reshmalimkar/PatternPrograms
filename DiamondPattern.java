90ae185214547a49d6723c8caaea7551ff4951f5
reshmalimkar <117263593+reshmalimkar@users.noreply.github.com>
2024-06-24 23:00:29 +0530

    Add files via upload

---
+++ DiamondPattern.java
@@ -0,0 +1,34 @@
+package com.jspider.practice.patterns;
+
+import java.util.Scanner;
+
+public class DiamondPattern {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		int n=sc.nextInt();
+		
+		for(int i=1;i<=n;i++) {
+			//space
+			for(int j=1;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		//lowerHalf
+		for(int i=n;i>=1;i--) {
+			for(int j=1;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
